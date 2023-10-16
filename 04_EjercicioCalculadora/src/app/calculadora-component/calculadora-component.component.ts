0da18a9f38bd0b3dea3b599b34a9415c25ea5529
Marco <marco.pertegal.prieto@gmail.com>
2023-10-16 18:54:25 +0200

    Calculadora

--- 04_EjercicioCalculadora/src/app/calculadora-component/calculadora-component.component.ts
+++ 04_EjercicioCalculadora/src/app/calculadora-component/calculadora-component.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora-component.component.css']
 })
 export class CalculadoraComponentComponent {
+  operacion = ''; // Variable para almacenar la operación actual.
 
+  agregarNumero(numero: string) {
+    this.operacion += numero; // Agregar el número a la operación actual.
+  }
+  calcular() {
+
+    // Dividir la operación en sus componentes (números y operadores).
+    if (this.operacion.includes('+')) {
+      const [num1, num2] = this.operacion.split('+').map(Number);
+      this.operacion = (num1 + num2).toString();
+    } else if (this.operacion.includes('-')) {
+      const [num1, num2] = this.operacion.split('-').map(Number);
+      this.operacion = (num1 - num2).toString();
+    } else if (this.operacion.includes('*')) {
+      const [num1, num2] = this.operacion.split('*').map(Number);
+      this.operacion = (num1 * num2).toString();
+    } else if (this.operacion.includes('/')) {
+      const [num1, num2] = this.operacion.split('/').map(Number);
+      if (num2 === 0) {
+        this.operacion = 'Error: División por cero';
+      } else {
+        this.operacion = (num1 / num2).toString();
+      }
+    }
+  }
+
+  borrar() {
+    this.operacion = '';
+  }
 }
