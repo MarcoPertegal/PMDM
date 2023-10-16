748b93ec0cd4829aa56239be7afc7fb41c362a24
Marco <marco.pertegal.prieto@gmail.com>
2023-10-16 19:03:37 +0200

    Calculardora documentada

--- 04_EjercicioCalculadora/src/app/calculadora-component/calculadora-component.component.ts
+++ 04_EjercicioCalculadora/src/app/calculadora-component/calculadora-component.component.ts
@@ -9,11 +9,13 @@ export class CalculadoraComponentComponent {
   operacion = ''; // Variable para almacenar la operación actual.
 
   agregarNumero(numero: string) {
-    this.operacion += numero; // Agregar el número a la operación actual.
+    this.operacion += numero; // Agregar el número a la operación actual lo concadena pq es un string.
   }
   calcular() {
-
     // Dividir la operación en sus componentes (números y operadores).
+    //this.operacion.split('+') convierte en un array de dos elementos y .map(Number) transfomra el array
+    //en dos numeros que se guardan en num1 y num2 se suman y se transforman a string y se vuelven a guardar 
+    //como string en la variable this.operacion
     if (this.operacion.includes('+')) {
       const [num1, num2] = this.operacion.split('+').map(Number);
       this.operacion = (num1 + num2).toString();
