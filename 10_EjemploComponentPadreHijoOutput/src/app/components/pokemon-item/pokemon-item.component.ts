570c30e142ea2b95351f46d4d299fcdcc0d4c989
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-10-24 14:27:52 +0200

    Hacer pokemon list

--- 10_EjemploComponentPadreHijoOutput/src/app/components/pokemon-item/pokemon-item.component.ts
+++ 10_EjemploComponentPadreHijoOutput/src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon-detail.interface';
 
 @Component({
   selector: 'app-pokemon-item',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-item.component.css']
 })
 export class PokemonItemComponent {
+  @Input() pokemon!: Pokemon;
+  @Output() pokemonClick = new EventEmitter<string>();
 
+  getPokemonImage() {
+    const pokemonId = this.pokemon.url.split('/')[6];
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+  }
+
+  viewPokemonDetail() {
+    this.pokemonClick.emit(this.pokemon.name);
+  }
 }
