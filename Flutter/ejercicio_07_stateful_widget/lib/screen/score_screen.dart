cd43056babf7774c03dc6261269c2f74e8314e95
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-01-15 13:33:41 +0100

    Errors

--- Flutter/ejercicio_07_stateful_widget/lib/screen/score_screen.dart
+++ Flutter/ejercicio_07_stateful_widget/lib/screen/score_screen.dart
@@ -8,8 +8,88 @@ class ScoreScreen extends StatefulWidget {
 }
 
 class _ScoreScreenState extends State<ScoreScreen> {
+  int _counter = 0;
+  bool _widgetSelected = false;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('aaaaa'),
+      ),
+      body: Center(
+        child: Container(
+          color: _widgetSelected ? Colors.red : Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(_counter == 1 ? 'vez' : 'veces'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _counter++;
+              _widgetSelected = true;
+            });
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add)),
+    );
   }
+
+  /*
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _widgetSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          color: _widgetSelected ? Colors.red : Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(_counter == 1 ? 'vez' : 'veces'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _counter++;
+            _widgetSelected = true;
+          });
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+  */
 }
