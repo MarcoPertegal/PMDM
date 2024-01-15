cab13393a56c70375c6464db0c73305af9933ab9
Marco <marco.pertegal.prieto@gmail.com>
2024-01-15 19:45:25 +0100

    Score screen base done, layout remaining

--- Flutter/ejercicio_07_stateful_widget/lib/screen/score_screen.dart
+++ Flutter/ejercicio_07_stateful_widget/lib/screen/score_screen.dart
@@ -8,88 +8,123 @@ class ScoreScreen extends StatefulWidget {
 }
 
 class _ScoreScreenState extends State<ScoreScreen> {
-  int _counter = 0;
-  bool _widgetSelected = false;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('aaaaa'),
-      ),
-      body: Center(
-        child: Container(
-          color: _widgetSelected ? Colors.red : Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(_counter == 1 ? 'vez' : 'veces'),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _counter++;
-              _widgetSelected = true;
-            });
-          },
-          tooltip: 'Increment',
-          child: const Icon(Icons.add)),
-    );
-  }
-
-  /*
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _widgetSelected = false;
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+  Color _backgroundColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Score Screen'),
       ),
       body: Center(
         child: Container(
-          color: _widgetSelected ? Colors.red : Colors.white,
+          color: _backgroundColor,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$teamAPoints',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  Text(
+                    '$teamBPoints',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _backgroundColor = _backgroundColor == Colors.white
+                            ? Colors.black
+                            : Colors.white;
+                      });
+                    },
+                    child: Text('Cambiar Color'),
+                  ),
+                ],
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(_counter == 1 ? 'vez' : 'veces'),
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-            _widgetSelected = true;
-          });
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamAPoints++;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("1"),
+              ),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamAPoints += 2;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("2"),
+              ),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamAPoints += 3;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("3"),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamBPoints++;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("1"),
+              ),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamBPoints += 2;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("2"),
+              ),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    teamBPoints += 3;
+                  });
+                },
+                tooltip: 'Increment',
+                child: const Text("3"),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
-}
-  */
 }
