065624e29f41365ec75152dce430302127355695
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-01-15 12:59:29 +0100

    Stateful example and base proyect for exercise 6

---
+++ Flutter/ejercicio_07_stateful_widget/android/app/src/main/kotlin/com/example/ejercicio_07_stateful_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ejercicio_07_stateful_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/example_05_flutter_stateful_widget/android/app/src/main/kotlin/com/example/example_05_flutter_stateful_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_05_flutter_stateful_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
