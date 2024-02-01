79fcb55332a519c1d01c1d54f8604cc99aff1583
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-02-01 14:22:49 +0100

    google maps chopper

---
+++ Flutter/f16_ejercicio_googlemaps_fuentesvalencia/lib/service/font_service.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FontService {
+  static const API = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/fonts-daigua-publica-fuentes-de-agua-publica/records?limit=20'
+  static const headers = {
+    'apikey: AIzaSyA7JAUk7IriI1bV0wmRR_GrbPjr-CgFKeM'
+  }
+}
