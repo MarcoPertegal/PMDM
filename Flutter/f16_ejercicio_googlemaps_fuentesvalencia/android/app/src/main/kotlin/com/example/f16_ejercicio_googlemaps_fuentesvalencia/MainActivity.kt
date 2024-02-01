a345c42d11c88a811c876610939f38fb524ec382
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-02-01 12:09:03 +0100

    dependencies added, min sdk version on build gradle, android manifest

---
+++ Flutter/f16_ejercicio_googlemaps_fuentesvalencia/android/app/src/main/kotlin/com/example/f16_ejercicio_googlemaps_fuentesvalencia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f16_ejercicio_googlemaps_fuentesvalencia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/f16_ejercicio_googlemaps_fuentesvalencia/lib/page/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:f16_ejercicio_googlemaps_fuentesvalencia/widget/map_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MapWidget();
+  }
+}
